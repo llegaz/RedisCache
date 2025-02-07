dd6cfea8a49fbac96501b654ec797bbfa5ccabb1
Laurent Legaz <laurent@legaz.eu>
2025-02-07 01:55:35 +0100

    exceptions handling and 1st Units

---
+++ src/Exception/InvalidValuesException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LLegaz\Cache\Exception;
+
+use Psr\Cache\InvalidArgumentException;
+
+class InvalidValuesException extends \Exception implements InvalidArgumentException
+{
+    public function __construct(string $message = 'RedisCache says "Can\'t do shit with those values"' . PHP_EOL, int $code = 400, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
