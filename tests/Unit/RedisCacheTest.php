553f7773ad7496cb20539ae90a3bf1b923a98bf8
Laurent Legaz <laurent@legaz.eu>
2025-10-10 11:42:29 +0200

    still a lot of work to be done on PSR-6 part

--- tests/Unit/RedisCacheTest.php
+++ tests/Unit/RedisCacheTest.php
@@ -12,10 +12,36 @@ namespace LLegaz\Cache\Tests\Unit;
 class RedisCacheTest extends SimpleCacheTest
 {
     /**
-     * @todo remove dummy
+     * @todo implement this
      */
-    public function testSomething()
+    public function testFetchFromPool()
     {
         $this->assertTrue(true);
     }
+
+    public function testHasInPool()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testGetAllCacheStoreAsString()
+    {
+        $this->assertTrue(true);
+    }
+
+/*
+
+    public function getAllCacheStoreAsArray()
+    public function getPoolKeys(string $pool): array
+    public function getInfo(): array
+    public function getTtl(string $key): int
+    public function printCacheKeys()
+    public function printCacheHash(string $pool, $silent = false): string
+    public function setHsetPoolExpiration(string $pool, int $expirationTime = self::HOURS_EXPIRATION_TIME): bool
+    public function unserializeFromPool(string $key, string $pool)
+    public function serializeToPool(string $key, mixed $data, string $pool): bool
+    public function deleteFromPool(array $keys, string $pool): bool
+    public function storeToPool(array $values, string $pool): bool
+    public function fetchAllFromPool(string $pool): array
+*/
 }
