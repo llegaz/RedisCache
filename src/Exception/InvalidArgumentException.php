b434f47868137e08d7e9e5045a8e235760707b91
Laurent Legaz <laurent@legaz.eu>
2025-10-16 12:15:58 +0200

    cs and fix getItems

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -11,7 +11,7 @@ use Psr\SimpleCache\InvalidArgumentException as Psr16InvalidArgumentException;
  * inspired from Symfony Cache (by Nicolas Grekas)
  *
  * PSR-6 and PSR-16 <b>InvalidArgumentException</b>
- * 
+ *
  * @todo rework / clean all package(s) exceptions ?
  */
 class InvalidArgumentException extends \InvalidArgumentException implements Psr6InvalidArgumentException, Psr16InvalidArgumentException
