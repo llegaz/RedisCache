5bd57c76c1fe4905bedf46eb3a72303f12ffef5f
Laurent Legaz <laurent@legaz.eu>
2025-12-25 19:16:44 +0100

    [security] testing key injection

--- tests/Functional/SecurityTest.php
+++ tests/Functional/SecurityTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use LLegaz\Cache\RedisCache as SUT;
+use LLegaz\Cache\Pool\CacheEntryPool;
+use LLegaz\Cache\RedisEnhancedCache as SUT2;
 
 /**
  * @todo test PSR 6 class too
@@ -13,17 +15,23 @@ use LLegaz\Cache\RedisCache as SUT;
 class SecurityTest extends \PHPUnit\Framework\TestCase
 {
     protected SUT $cache;
-    
+
+    protected CacheEntryPool $pool;
+
     protected function setUp(): void {
         parent::setUp();
 
         $this->cache = new SUT();
+        $this->pool = new CacheEntryPool(new SUT2());
+
+        $this->cache->clear();
+        $this->pool->clear();
     }
     /**
      * Security test: Ensure special characters in keys don't cause
      * command injection or unexpected behavior.
      */
-    public function testSpecialCharactersDoNotCauseInjection()
+    public function testSpecialCharactersDoNotCauseInjectionPSR16()
     {
         // Attempt "injection-like" patterns
         $dangerousKeys = [
@@ -44,5 +52,33 @@ class SecurityTest extends \PHPUnit\Framework\TestCase
 
         // Verify no side effects (cache not flushed)
         $this->assertEquals('chirp', $this->cache->get('canary'));
+    }
+
+    public function testSpecialCharactersDoNotCauseInjectionPSR6()
+    {
+        
+        // Attempt "injection-like" patterns
+        $dangerousKeys = [
+            'key\nFLUSHALL',
+            'key|FLUSHALL',
+            'key`FLUSHALL`',
+            'key$(FLUSHALL)',
+        ];
+
+        $item = $this->pool->getItem('canary');
+        $item->set('chirp');
+        $this->pool->save($item);
+
+        foreach ($dangerousKeys as $key) {
+            // Should work without executing any injection
+            $item = $this->pool->getItem($key);
+            $item->set('safe value!');
+            $this->pool->save($item);
+            $this->assertEquals('safe value!', $this->pool->getItem($key)->get());
+            $this->pool->deleteItem($key);
+        }
+
+        // Verify no side effects (cache not flushed)
+        $this->assertEquals('chirp', $this->pool->getItem('canary')->get());
     }
 }
